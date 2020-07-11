5153478dd11a173fecb41f0bfa14144dbb5d977a
me6edi <exited17@gmail.com>
2020-07-11 21:20:08 +0600

    play videos section edite

--- PSD to HTML  Part - 1/assets/js/main.js
+++ PSD to HTML  Part - 1/assets/js/main.js
@@ -10,4 +10,18 @@ $(document).ready(function($){
     //$( ".owl-prev").html('<i class="fa screenshort-arow fa-chevron-left"></i>');
     //$( ".owl-next").html('<i class="fa screenshort-arow fa-chevron-right"></i>');
 
+    //Magnific Poup
+    $('#Youtube-video').magnificPopup({
+        type:'iframe',
+        iframe: {
+            patterns: {
+              youtube: {
+                index: 'youtube.com/',
+                id: 'v=',
+                src: 'http://www.youtube.com/embed/%id%?autoplay=1'
+              }
+            },
+            srcAction: 'iframe_src',
+          }
+    });
 }(jQuery));
