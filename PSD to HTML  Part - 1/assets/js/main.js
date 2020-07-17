d7213c598b44d425464172ef9aab5fcb8e31aff2
me6edi <exited17@gmail.com>
2020-07-17 16:22:59 +0600

    Counter section complite

--- PSD to HTML  Part - 1/assets/js/main.js
+++ PSD to HTML  Part - 1/assets/js/main.js
@@ -30,4 +30,17 @@ $(document).ready(function($){
         // options here
        });
 
+    //Counter
+       $('.counter-number').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
 }(jQuery));
